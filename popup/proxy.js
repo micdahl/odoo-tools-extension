ba77e9823217060ea5e2ffc3c11a45288441fed6
Michael Dahl <michael@dahl.software>
2023-11-13 11:14:47 +0100

    add support for Odoo 17.0

--- popup/proxy.js
+++ popup/proxy.js
@@ -2,7 +2,11 @@ window.addEventListener("message", function(event){
     if(event.data.hasOwnProperty("event") && event.data.event == "odooToolsBarcodeMessage")
     {
         if (typeof odoo != "undefined") {
-            odoo.__DEBUG__.services['web.core'].bus.trigger('barcode_scanned', event.data.value, $(".o_web_client")[0]);
+            if (typeof odoo.__DEBUG__ != "undefined") {
+                odoo.__DEBUG__.services['web.core'].bus.trigger('barcode_scanned', event.data.value, $(".o_web_client")[0]);
+            } else if (typeof odoo.__WOWL_DEBUG__ != "undefined") { 
+                odoo.__WOWL_DEBUG__.root.env.services.barcode.bus.trigger("barcode_scanned", {barcode: event.data.value});
+            }
         }
         else {
             console.log("Barcodes are only available for Odoo");
