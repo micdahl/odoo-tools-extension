9bad538a5844a22122064939a278845421258b8e
Michael Dahl <michael@dahl.software>
2024-01-04 12:04:26 +0100

    Add sha256 to content policy

--- popup/contentScript.js
+++ popup/contentScript.js
@@ -1,3 +1,7 @@
+var inject = document.createElement("script");
+inject.src=chrome.runtime.getURL("popup/proxy.js");
+(document.head||document.documentElement).appendChild(inject);
+
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse) {
     window.postMessage({ "event": "odooToolsBarcodeMessage", "value": message.barcode });
